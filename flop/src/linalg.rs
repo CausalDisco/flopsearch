f9219bbcb7d3a8b8df482f9f51eb7dc223469f44
Marcel Wienöbst <marcel.wienoebst@gmx.de>
2025-10-18 00:02:40 +0200

    further polish and improvements

--- flop/src/linalg.rs
+++ flop/src/linalg.rs
@@ -1,6 +1,4 @@
-// TODO: for now work without explicit buffers
-// -> rename buffer to data
-// -> later check how much allocs cost
+use std::mem::MaybeUninit;
 
 pub struct SquareMatrix {
     data: Vec<f64>,
@@ -20,7 +18,8 @@ impl SquareMatrix {
         self.data[i * self.dim + j] = x;
     }
 
-    // TODO: add error handling for degenerate matrices
+    // not really used much currently, only for trivial initial Cholesky
+    // TODO: maybe polish a bit
     pub fn cholesky(&self) -> Cholesky {
         // create Cholesky row-by-row in square matrix
         let mut tmp_chol = SquareMatrix::new(vec![0.0; self.dim * self.dim], self.dim);
@@ -39,7 +38,7 @@ impl SquareMatrix {
         }
 
         // convert to packed format
-        let mut chol_data = Vec::with_capacity(Cholesky::chol_size(self.dim));
+        let mut chol_data = Vec::with_capacity(self.dim * (self.dim + 1) / 2);
         for i in 0..self.dim {
             for j in i..self.dim {
                 chol_data.push(tmp_chol.get(j, i));
@@ -49,26 +48,7 @@ impl SquareMatrix {
     }
 }
 
-fn abs_diff_sum(a: &[f64], b: &[f64]) -> f64 {
-    let mut sum = 0.0;
-    for i in 0..a.len() {
-        sum += (a[i] - b[i]).abs();
-    }
-    sum
-}
-
-#[test]
-fn test_cholesky() {
-    let input = SquareMatrix::new(
-        vec![4.0, 12.0, -16.0, 12.0, 37.0, -43.0, -16.0, -43.0, 98.0],
-        3,
-    );
-    let chol = Cholesky::new(vec![2.0, 6.0, -8.0, 1.0, 5.0, 3.0], 3);
-    let output = input.cholesky();
-    assert!(abs_diff_sum(&chol.data, &output.data) < 1e-9);
-}
-
-// packed format column-major
+// packed column-major format
 #[derive(Clone, Debug)]
 pub struct Cholesky {
     pub data: Vec<f64>,
@@ -83,7 +63,7 @@ impl Cholesky {
     // this is one of the hot loops
     #[inline(always)]
     fn forward_solve(&self, x: &mut [f64]) {
-        // this unsafe code is very slightly faster than the safe version below
+        // this unsafe code is very slightly faster than a safe version
         unsafe {
             let mut data_ptr = self.data.as_ptr();
             let x_ptr = x.as_mut_ptr();
@@ -106,20 +86,6 @@ impl Cholesky {
                 }
             }
         }
-
-        // safe version of the code above
-        // let mut idx = 0;
-        // for i in 0..self.dim {
-        //     x[i] /= self.data[idx];
-        //     idx += 1;
-        //     let mult = x[i];
-        //     let lhs = &mut x[i + 1..self.dim];
-        //     let rhs = &self.data[idx..idx - i - 1 + self.dim];
-        //     for j in 0..lhs.len() {
-        //         lhs[j] -= rhs[j] * mult;
-        //     }
-        //     idx += self.dim - i - 1;
-        // }
     }
 
     #[inline(always)]
@@ -148,11 +114,10 @@ impl Cholesky {
     }
 
     pub fn insert_column_before_last(&self, mut x: Vec<f64>) -> Self {
-        // allocate new Cholesky
-        let new_size = Self::chol_size(self.dim + 1);
-        let mut new_chol = Self::new(Vec::with_capacity(new_size), self.dim + 1);
+        let new_size = (self.dim + 1) * (self.dim + 2) / 2;
+        let mut new_data: Vec<MaybeUninit<f64>> = Vec::with_capacity(new_size);
         unsafe {
-            new_chol.data.set_len(new_size);
+            new_data.set_len(new_size);
         }
 
         // solve for x (added row if it would be appended)
@@ -178,23 +143,36 @@ impl Cholesky {
         let mut idx = 0;
         for i in 0..self.dim {
             let stride = self.dim - i - 1;
-            new_chol.data[idx + i..idx + i + stride].copy_from_slice(&self.data[idx..idx + stride]);
-            idx += stride;
-            new_chol.data[idx + i] = x[i];
-            new_chol.data[idx + i + 1] = self.data[idx];
-            idx += 1;
+
+            // SAFETY: write to valid, unitialized elements
+            unsafe {
+                let dst = new_data.as_mut_ptr().add(idx + i);
+                let src = self.data.as_ptr().add(idx);
+
+                std::ptr::copy_nonoverlapping(src, dst as *mut f64, stride);
+                *dst.add(stride) = MaybeUninit::new(x[i]);
+                *dst.add(stride + 1) = MaybeUninit::new(self.data[idx + stride]);
+            }
+            idx += stride + 1;
         }
-        new_chol.data[new_size - 2] = new_left_of_corner;
-        new_chol.data[new_size - 1] = new_corner.abs();
-        new_chol
+        // SAFETY: overwrite at new_size - 2 and write new_size - 1 (last element)
+        unsafe {
+            *new_data.get_unchecked_mut(new_size - 2) = MaybeUninit::new(new_left_of_corner);
+            *new_data.get_unchecked_mut(new_size - 1) = MaybeUninit::new(new_corner.abs());
+        }
+
+        // SAFETY: all elements are initialized
+        Self::new(
+            unsafe { std::mem::transmute::<Vec<MaybeUninit<f64>>, Vec<f64>>(new_data) },
+            self.dim + 1,
+        )
     }
 
     pub fn remove_column(&self, k: usize) -> Self {
-        // allocate new Cholesky
-        let new_size = Self::chol_size(self.dim - 1);
-        let mut new_chol = Self::new(Vec::with_capacity(new_size), self.dim - 1);
+        let new_size = (self.dim - 1) * self.dim / 2;
+        let mut new_data: Vec<MaybeUninit<f64>> = Vec::with_capacity(new_size);
         unsafe {
-            new_chol.data.set_len(new_size);
+            new_data.set_len(new_size);
         }
 
         // column vector that needs to be zeroed with Givens rotations
@@ -203,14 +181,17 @@ impl Cholesky {
         let mut idx = 0;
         for i in 0..self.dim {
             if i < k {
-                let stride = k - i;
-                new_chol.data[idx - i..idx - i + stride]
-                    .copy_from_slice(&self.data[idx..idx + stride]);
-                idx += stride + 1;
-                let stride = self.dim - k - 1;
-                new_chol.data[idx - i - 1..idx - i - 1 + stride]
-                    .copy_from_slice(&self.data[idx..idx + stride]);
-                idx += stride;
+                // SAFETY: write to valid, unitialized elements
+                unsafe {
+                    let stride = k - i;
+                    let dst = new_data.as_mut_ptr().add(idx - i);
+                    let src = self.data.as_ptr().add(idx);
+                    std::ptr::copy_nonoverlapping(src, dst as *mut f64, stride);
+                    let dst = dst.add(stride);
+                    let src = src.add(stride + 1);
+                    std::ptr::copy_nonoverlapping(src, dst as *mut f64, self.dim - k - 1);
+                }
+                idx += self.dim - i;
             } else if i == k {
                 // skip (k, k) element
                 idx += 1;
@@ -219,7 +200,10 @@ impl Cholesky {
                 idx += stride;
             } else {
                 let (c, s, r) = Self::make_givens(self.data[idx], x[i - k - 1]);
-                new_chol.data[idx - self.dim] = r;
+                // SAFETY: write diagonal element
+                unsafe {
+                    *new_data.get_unchecked_mut(idx - self.dim) = MaybeUninit::new(r);
+                }
                 x[i - k - 1] = 0.0;
                 idx += 1;
 
@@ -227,66 +211,93 @@ impl Cholesky {
                 for j in i + 1..self.dim {
                     let tau1 = self.data[idx];
                     let tau2 = x[j - k - 1];
-                    new_chol.data[idx - self.dim] = c * tau1 - s * tau2;
+                    // SAFETY: write below diagonal
+                    unsafe {
+                        *new_data.get_unchecked_mut(idx - self.dim) =
+                            MaybeUninit::new(c * tau1 - s * tau2);
+                    }
                     x[j - k - 1] = s * tau1 + c * tau2;
                     idx += 1;
                 }
             }
         }
-        new_chol
+
+        // SAFETY: all elements initialized
+        Self::new(
+            unsafe { std::mem::transmute::<Vec<MaybeUninit<f64>>, Vec<f64>>(new_data) },
+            self.dim - 1,
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn abs_diff_sum(a: &[f64], b: &[f64]) -> f64 {
+        let mut sum = 0.0;
+        for i in 0..a.len() {
+            sum += (a[i] - b[i]).abs();
+        }
+        sum
     }
 
-    // put into utils or something -> or just put directly in the functions tbh
-    #[inline(always)]
-    fn chol_size(dim: usize) -> usize {
-        dim * (dim + 1) / 2
+    #[test]
+    fn test_cholesky() {
+        let input = SquareMatrix::new(
+            vec![4.0, 12.0, -16.0, 12.0, 37.0, -43.0, -16.0, -43.0, 98.0],
+            3,
+        );
+        let chol = Cholesky::new(vec![2.0, 6.0, -8.0, 1.0, 5.0, 3.0], 3);
+        let output = input.cholesky();
+        assert!(abs_diff_sum(&chol.data, &output.data) < 1e-9);
     }
-}
 
-#[test]
-fn test_cholesky_updates() {
-    let input = SquareMatrix::new(
-        vec![4.0, 12.0, -16.0, 12.0, 37.0, -43.0, -16.0, -43.0, 98.0],
-        3,
-    );
-    let chol = Cholesky::new(vec![2.0, 6.0, -8.0, 1.0, 5.0, 3.0], 3);
-    let output = input.cholesky();
-    assert!(abs_diff_sum(&chol.data, &output.data) < 1e-9);
+    #[test]
+    fn test_cholesky_removals() {
+        let input = SquareMatrix::new(
+            vec![4.0, 12.0, -16.0, 12.0, 37.0, -43.0, -16.0, -43.0, 98.0],
+            3,
+        );
+        let chol = Cholesky::new(vec![2.0, 6.0, -8.0, 1.0, 5.0, 3.0], 3);
+        let output = input.cholesky();
+        assert!(abs_diff_sum(&chol.data, &output.data) < 1e-9);
 
-    let out_c = output.remove_column(2);
-    let chol_c = Cholesky::new(vec![2.0, 6.0, 1.0], 3);
-    assert!(abs_diff_sum(&chol_c.data, &out_c.data) < 1e-9);
+        let out_rem2 = output.remove_column(2);
+        let chol_rem2 = Cholesky::new(vec![2.0, 6.0, 1.0], 3);
+        assert!(abs_diff_sum(&chol_rem2.data, &out_rem2.data) < 1e-9);
 
-    let out_d = output.remove_column(1);
-    let chol_d = Cholesky::new(vec![2.0, -8.0, 34.0f64.sqrt()], 2);
-    assert!(abs_diff_sum(&chol_d.data, &out_d.data) < 1e-9);
+        let out_rem1 = output.remove_column(1);
+        let chol_rem1 = Cholesky::new(vec![2.0, -8.0, 34.0f64.sqrt()], 2);
+        assert!(abs_diff_sum(&chol_rem1.data, &out_rem1.data) < 1e-9);
 
-    let out_e = output.remove_column(0);
-    let chol_e = Cholesky::new(
-        vec![
-            37.0f64.sqrt(),
-            -43.0 * 37.0f64.sqrt() / 37.0,
-            65749.0f64.sqrt() / 37.0,
-        ],
-        2,
-    );
-    assert!(abs_diff_sum(&chol_e.data, &out_e.data) < 1e-9);
-}
+        let out_rem0 = output.remove_column(0);
+        let chol_rem0 = Cholesky::new(
+            vec![
+                37.0f64.sqrt(),
+                -43.0 * 37.0f64.sqrt() / 37.0,
+                65749.0f64.sqrt() / 37.0,
+            ],
+            2,
+        );
+        assert!(abs_diff_sum(&chol_rem0.data, &out_rem0.data) < 1e-9);
+    }
 
-#[test]
-fn test_cholesky_insert_before_last() {
-    let a = SquareMatrix::new(vec![4.0, -16.0, -16.0, 98.0], 2);
-    let out_a = a.cholesky();
-    let new_chol = out_a.insert_column_before_last(vec![12.0, -43.0, 37.0]);
-    let chol = Cholesky::new(vec![2.0, 6.0, -8.0, 1.0, 5.0, 3.0], 3);
-    assert!(abs_diff_sum(&chol.data, &new_chol.data) < 1e-9);
-}
+    #[test]
+    fn test_cholesky_insert_before_last_2by2() {
+        let input = SquareMatrix::new(vec![4.0, -16.0, -16.0, 98.0], 2);
+        let output = input.cholesky();
+        let new_chol = output.insert_column_before_last(vec![12.0, -43.0, 37.0]);
+        let true_chol = Cholesky::new(vec![2.0, 6.0, -8.0, 1.0, 5.0, 3.0], 3);
+        assert!(abs_diff_sum(&true_chol.data, &new_chol.data) < 1e-9);
+    }
 
-#[test]
-fn test_cholesky_insert_before_last2() {
-    let a = SquareMatrix::new(vec![37.0], 1);
-    let out_a = a.cholesky();
-    let new_chol = out_a.insert_column_before_last(vec![12.0, 4.0]);
-    let chol = Cholesky::new(vec![2.0, 6.0, 1.0], 2);
-    assert!(abs_diff_sum(&chol.data, &new_chol.data) < 1e-9);
+    #[test]
+    fn test_cholesky_insert_before_last_1by1() {
+        let input = SquareMatrix::new(vec![37.0], 1);
+        let output = input.cholesky();
+        let new_chol = output.insert_column_before_last(vec![12.0, 4.0]);
+        let true_chol = Cholesky::new(vec![2.0, 6.0, 1.0], 2);
+        assert!(abs_diff_sum(&true_chol.data, &new_chol.data) < 1e-9);
+    }
 }
