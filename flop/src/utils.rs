ba973ac76e399dd14303f2cba29387a8ea3f5792
Marcel Wienöbst <marcel.wienoebst@gmx.de>
2025-10-24 12:00:32 +0200

    remove TODO

--- flop/src/utils.rs
+++ flop/src/utils.rs
@@ -16,7 +16,6 @@ pub fn cov_matrix(data: &DMatrix<f64>) -> DMatrix<f64> {
     (centered_data.transpose() * centered_data) / n as f64
 }
 
-// TODO: have tests here
 pub fn corr_matrix(data: &DMatrix<f64>) -> DMatrix<f64> {
     let mut cov = cov_matrix(data);
     let std_devs = cov.diagonal().map(|x| x.sqrt());
